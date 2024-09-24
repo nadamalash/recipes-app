d352ae7e0b72404ff90ecf21e18c708badd09eb2
nadamalash <nadamalash58@gmail.com>
2024-09-24 15:37:21 +0300

    Navbar and hero section are created

--- src/app/layout/additions/navbar/navbar.component.ts
+++ src/app/layout/additions/navbar/navbar.component.ts
@@ -1,10 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
-
 }
